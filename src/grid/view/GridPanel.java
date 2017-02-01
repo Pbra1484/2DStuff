6bc1b58844c3b80495139bf728e7af0b362e0767
Pbra1484 <pbra1484@740s-pl-123.local>
2017-02-01 12:38:48 -0700

    added all current components to the panel and designed GUI

--- src/grid/view/GridPanel.java
+++ src/grid/view/GridPanel.java
@@ -14,6 +14,7 @@ public class GridPanel extends JPanel
 	private JLabel rowsLabel;
 	private JLabel colsLabel;
 	private JLabel titleLabel;
+	private JButton updateButton;
 	
 	private JTable gridTable;
 	private JScrollPane gridPane;
@@ -25,10 +26,16 @@ public class GridPanel extends JPanel
 		baseLayout = new SpringLayout();
 		rows = new JTextField(10);
 		cols = new JTextField(10);
+
 		info = new JTextField(10);
+		
 		rowsLabel = new JLabel("Row");
+		
 		colsLabel = new JLabel("Column");
+		
 		titleLabel = new JLabel("Title");
+		updateButton = new JButton("Update");
+		
 		
 		setupTable();
 		setupPanel();
@@ -42,6 +49,10 @@ public class GridPanel extends JPanel
 		gridTable = new JTable();
 		gridTable.setModel(data);
 		gridPane = new JScrollPane();
+		baseLayout.putConstraint(SpringLayout.NORTH, gridPane, 10, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, gridPane, 160, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, gridPane, -10, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, gridPane, -10, SpringLayout.EAST, this);
 		gridPane.setViewportView(gridTable);
 	}
 	
@@ -54,10 +65,22 @@ public class GridPanel extends JPanel
 		this.add(rowsLabel);
 		this.add(colsLabel);
 		this.add(titleLabel);
-		
+		this.add(updateButton);
+		this.add(gridPane);
 	}
 	private void setupLayout()
 	{
+		baseLayout.putConstraint(SpringLayout.WEST, rows, 0, SpringLayout.WEST, cols);
+		baseLayout.putConstraint(SpringLayout.WEST, cols, 0, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, cols, -228, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, info, 33, SpringLayout.SOUTH, rows);
+		baseLayout.putConstraint(SpringLayout.WEST, info, 0, SpringLayout.WEST, rows);
+		baseLayout.putConstraint(SpringLayout.NORTH, rows, 8, SpringLayout.SOUTH, rowsLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, rowsLabel, 6, SpringLayout.SOUTH, cols);
+		baseLayout.putConstraint(SpringLayout.WEST, rowsLabel, 0, SpringLayout.WEST, cols);
+		baseLayout.putConstraint(SpringLayout.WEST, colsLabel, 0, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, colsLabel, -6, SpringLayout.NORTH, cols);baseLayout.putConstraint(SpringLayout.NORTH, updateButton, 6, SpringLayout.SOUTH, info);
+		baseLayout.putConstraint(SpringLayout.WEST, updateButton, 0, SpringLayout.WEST, this);
 		
 	}
 	private  void setupListeners()
